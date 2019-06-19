5d00c87c70da43b3f0add1f7783a5d9ae2bf73cf
Pedro Amaro <l42348@alunos.uevora.pt>
2019-06-19 18:19:12 +0100

    Input em produção

--- EDA2_FlyMeToTheMoon/main.c
+++ EDA2_FlyMeToTheMoon/main.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include "airTable.h"
 
+#define ID_AIR_SIZE 5
+#define ID_VOO_SIZE 7
 
 //TR <aeroporto-partida> <aeroporto-destino> <hora-chegada-aeroporto>
 void calcViagem(char IdAirPartida[5],char IdAirChegada[5],short hourPartida,short minutePartida)
@@ -42,6 +44,7 @@ void intrudAir(char key[5],short hLocal,short mLocal)//recebe como argumento cod
     novoAir->hour=hLocal;
     novoAir->minute=mLocal;
     check = insert(novoAir);
+    printf("HORA %hd  , minuto %hd ",novoAir->hour,novoAir->minute);
     if (check)
     {
         printf("+ novo aeroporto %s\n",novoAir->Id);
@@ -56,41 +59,114 @@ int main(void)
     //testar hastable
     dummyItem =  malloc(sizeof(struct air));
     dummyItem->flag = true; //foi "removido"
-    
-    //5 Aeroportos de teste
-    struct air *itemA = malloc(sizeof(struct air));
-    char  arrA[5] = "LISB";
-    strcpy(itemA->Id,arrA);
-    itemA->flag =false;
-    insert(itemA);
-    struct air *itemB = malloc(sizeof(struct air));
-    char arrB[5] = "MADR";
-    strcpy(itemB->Id,arrB);
-    itemB->flag =false;
-    insert(itemB);
-    struct air *itemC =  malloc(sizeof(struct air));
-    char arrC[5] = "NY";
-    strcpy(itemC->Id,arrC);
-    itemC->flag =false;
-    insert(itemC);
-    struct air *itemD =  malloc(sizeof(struct air));
-    char arrD[5] = "PORT";
-    strcpy(itemD->Id,arrD);
-    itemD->flag =false;
-    insert(itemD);
-    struct air *itemE =  malloc(sizeof(struct air));
-    char arrE[5] = "LIBA";
-    strcpy(itemE->Id,arrE);
-    itemE->flag =false;
-    insert(itemE);
-
-    item = search("PORT");
-
-    if(item != NULL) {
-        printf("Element found: %s\n", item->Id);
-    } else {
-        printf("Element not found\n");
+    char cod1='\0';
+    char cod2='\0';
+    char idAir[ID_AIR_SIZE]="";//Declara e inicializa o array de chars a 0
+    char idVoo[ID_VOO_SIZE]="";//Declara e inicializa o array de chars a 0
+    char idAirPartida[ID_AIR_SIZE]="";//Declara e inicializa o array de chars a 0
+    char idAirDestino[ID_AIR_SIZE]="";//Declara e inicializa o array de chars a 0
+    short hLocal=0;
+    short mLocal=0;
+    short duracaoVoo=0;
+    //AI LIS 00:00
+   
+    while(scanf("%c%c ",&cod1,&cod2)!=EOF)// enquanto houver mais linhas
+    {
+        if (cod1=='X')
+        {
+            //Termina o programa
+            return -1;
+        }else if (cod1=='A' && cod2 == 'I')
+        {
+            
+            for (int i = 0; i < ID_AIR_SIZE-1; i++)
+            {
+                scanf("%c",&idAir[i]);
+                if (idAir[i]==' ')
+                {
+                    break;
+                }
+                
+            }
+            scanf(" %hd:%hd",&hLocal,&mLocal);
+            intrudAir(idAir,hLocal,mLocal);
+        }else if (cod1=='F' && cod2 == 'I')
+        {
+             //FI IB3111 PDL MAD 09:50 80
+            for (int i = 0; i < ID_VOO_SIZE-1; i++)
+            {
+                scanf("%c",&idVoo[i]);
+                if (idVoo[i]==' ')
+                {
+                    idVoo[i]='\0';
+                    break;
+                }
+                
+            }
+            for (int i = 0; i < ID_AIR_SIZE-1; i++)//regista o aeroporto de Partida
+            {
+                scanf("%c",&idAirPartida[i]);
+                printf("I = %d  char = %c",i,idAirPartida[i]);
+                if (idAirPartida[i]==' ')
+                {
+                    idAirPartida[i]='\0';
+                    break;
+                }
+                
+            }
+            for (int i = 0; i < ID_AIR_SIZE-1; i++)//regista o aeroporto de Chegada
+            {
+                scanf("%c",&idAirDestino[i]);
+                if (idAirDestino[i]==' ')
+                {
+                    break;
+                }
+                
+            }
+            scanf(" %hd:%hd %hd",&hLocal,&mLocal,&duracaoVoo);
+            printf("idvoo %s%s C %s hLocal = %hd mLocal= %hd duraçao= %hd",idVoo,idAirPartida,idAirDestino,hLocal,mLocal,duracaoVoo);
+        }
+        
+        
+        
+ 
     }
+
+
+    // //5 Aeroportos de teste
+    // struct air *itemA = malloc(sizeof(struct air));
+    // char  arrA[5] = "LISB";
+    // strcpy(itemA->Id,arrA);
+    // itemA->flag =false;
+    // insert(itemA);
+    // struct air *itemB = malloc(sizeof(struct air));
+    // char arrB[5] = "MADR";
+    // strcpy(itemB->Id,arrB);
+    // itemB->flag =false;
+    // insert(itemB);
+    // struct air *itemC =  malloc(sizeof(struct air));
+    // char arrC[5] = "NY";
+    // strcpy(itemC->Id,arrC);
+    // itemC->flag =false;
+    // insert(itemC);
+    // struct air *itemD =  malloc(sizeof(struct air));
+    // char arrD[5] = "PORT";
+    // strcpy(itemD->Id,arrD);
+    // itemD->flag =false;
+    // insert(itemD);
+    // struct air *itemE =  malloc(sizeof(struct air));
+    // char arrE[5] = "LIBA";
+    // strcpy(itemE->Id,arrE);
+    // itemE->flag =false;
+    // insert(itemE);
+
+    // item = search("PORT");
+
+    // if(item != NULL) {
+    //     printf("Element found: %s\n", item->Id);
+    // } else {
+    //     printf("Element not found\n");
+    // }
 
     // delete(item);
     // item = search("LISB");
